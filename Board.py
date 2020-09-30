6eb825a47154d2043ae1b85894849b93c7e0c182
Maxime MARTENS <contact@maximemartens.fr>
2020-10-01 01:09:57 +0200

    added code in python for board updates

--- Board.py
+++ Board.py
@@ -1,15 +1,60 @@
+from enum import IntEnum
+from random import randint
+
+class CellState(IntEnum):
+    empty = 0
+    living = 1
+    dead = 2
+
 class Board:
 
     def __init__(self):
         self.width = 25
         self.height = 25
-        self.grid = []
+        self.grid = [[(CellState.empty, CellState.living)[randint(0, 2) == 2] for x in range(self.width)] for y in range(self.height)]
+
+    def update(self):
         for y in range(self.height):
-            self.grid.append([])
+            for x in range(self.width):
+                if self.grid[x][y] == CellState.living:
+                    self.updateCell(x, y)
+
+    def updateCell(self, x, y):
+        state = self.grid[x][y]
+        neighbours = []
+        try:
+            for ry in range(y-1, y+1):
+                neighbours.append(self.grid[x-1][ry])
+                if ry != y:
+                    neighbours.append(self.grid[x][ry])
+                neighbours.append(self.grid[x+1][ry])
+        except IndexError:
+            a=0
+            #Do nothing
+        aliveNeighbours = 0
+        deadNeighbours = 0
+        for i in range(len(neighbours)):
+            if neighbours[i] == CellState.living:
+                aliveNeighbours += 1
+            if neighbours[i] == CellState.dead:
+                deadNeighbours += 1
+        if state == CellState.living:
+            if aliveNeighbours == 2 or aliveNeighbours == 3:
+                self.grid[x][y] = CellState.living
+            else:
+                self.grid[x][y] = CellState.dead
+        elif state == CellState.dead:
+            if aliveNeighbours == 3:
+                self.grid[x][y] = CellState.living
+            else:
+                self.grid[x][y] = CellState.empty
 
 
     def draw(self):
         for y in range(self.height):
             for x in range(self.width):
-                print(".", end='')
+                if self.grid[x][y] == CellState.empty:
+                    print(".", end="")
+                if self.grid[x][y] == CellState.living:
+                    print("O", end="")
             print("") #newline
--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 from Board import Board
+import time
+import sys
+
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def writelines(self, datas):
+       self.stream.writelines(datas)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+sys.stdout = Unbuffered(sys.stdout)
+
+#  Any live cell with two or three live neighbours survives.
+#  Any dead cell with three live neighbours becomes a live cell.
+#  All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 
 board = Board()
 board.draw()
+while 1:
+  time.sleep(1)
+  print("") #Skip line
+  board.update()
+  board.draw()
